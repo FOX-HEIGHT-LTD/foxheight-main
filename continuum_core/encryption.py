f5ac0a9e694af4208dc46d784c1a00ad75a36c26
Samson Mobisa <92422271+samsonmobisa@users.noreply.github.com>
2026-01-18 23:05:26 +0300

    Add FGTM to TMFG encryption and decryption functions

---
+++ continuum_core/encryption.py
@@ -0,0 +1,9 @@
+def fgtm_to_tmfg(data):
+    # Encrypt data from FGTM to TMFG
+    # Placeholder for encryption logic
+    return data[::-1]  # Simple reversal as example
+
+def tmfg_to_fgtm(data):
+    # Decrypt data from TMFG to FGTM
+    # Placeholder for decryption logic
+    return data[::-1]  # Simple reversal as example
